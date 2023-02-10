f2fc23c0ed5e6c469f72eabc142a4eb30f9ffcc4
littlephony <uladzislau.lukashevich@gmail.com>
2023-02-10 16:30:59 +0100

    Added base models and validation layer.

---
+++ library/src/tic_tac_toe/logic/models.py
@@ -0,0 +1,43 @@
+# tic_tac_toe/logic/models.py
+
+from __future__ import annotations
+import enum
+import re
+from dataclasses import dataclass
+from functools import cached_property
+
+class Mark(str, enum.Enum):
+    CROSS = 'X'
+    NAUGHT = 'O'
+
+    @property
+    def other(self) -> Mark:
+        return Mark.CROSS if self is Mark.NAUGHT else Mark.NAUGHT
+    
+@dataclass(frozen=True)
+class Grid:
+    cells: str = " " * 9
+
+    def __post_init__(self) -> None:
+        if not re.match(r"^[\sXO]{9}$", self.cells):
+            raise ValueError('Must contain 9 cells of: X, O, or space')
+        
+    @cached_property
+    def x_count(self) -> int:
+        return self.cells.count('X')
+    
+    @cached_property
+    def o_count(self) -> int:
+        return self.cells.count('O')
+    
+    @cached_property
+    def empty_count(self) -> int:
+        return self.cells.count(' ')
+    
+    
+        
+
+
+
+
+
