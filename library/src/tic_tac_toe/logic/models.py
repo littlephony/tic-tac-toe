d917e1a261c70ba525bd826adc010f3db9ead967
littlephony <uladzislau.lukashevich@gmail.com>
2023-02-11 10:43:02 +0100

    Added a minimax player

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -2,11 +2,12 @@
 
 from __future__ import annotations
 import enum
+import random
 import re
 from dataclasses import dataclass
 from functools import cached_property
 
-from tic_tac_toe.logic.exceptions import InvalidMove
+from tic_tac_toe.logic.exceptions import InvalidMove, UnknownGameScore
 from tic_tac_toe.logic.validators import validate_grid, validate_game_state
 
 WINNING_PATTERNS = (
@@ -106,6 +107,12 @@ class GameState:
                 moves.append(self.make_move_to(match.start()))
         return moves
     
+    def make_random_move(self) -> Move | None:
+        try:
+            return random.choice(self.possible_moves)
+        except IndexError:
+            return None
+
     def make_move_to(self, index: int) -> Move:
         if self.grid.cells[index] != ' ':
             raise InvalidMove('Cell is not empty')
@@ -122,6 +129,16 @@ class GameState:
                 self.starting_mark,
             ),
         )
+
+    def evaluate_score(self, mark: Mark) -> int:
+        if self.game_over:
+            if self.tie:
+                return 0
+            if self.winner is mark:
+                return 1
+            else:
+                return -1
+        raise UnknownGameScore('Game is not over yet')
     
 
 
